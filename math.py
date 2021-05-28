38a82f053b1d4b458ace89eac8f29a0ec83d92c2
saurabhghumnar <saurabh1213.sg@gmail.com>
2021-05-28 15:01:19 +0530

    Subtract function implemented

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y     #Master commit
 #Subtract implementation    
 def subtract(x,y):
-    pass
+    return x-y   #Bug123 commit
 #Multiply Implementation
 def multiply(x,y):
     pass
